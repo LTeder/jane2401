ef47f1daa4a6b5fee574368f40f68fd87402a1e4
Logan Teder <l.teder@columbia.edu>
2024-01-28 12:07:13 -0500

    Added score determination

--- src/fsquares.rs
+++ src/fsquares.rs
@@ -5,25 +5,24 @@ use std::fmt;
 use self::ndarray::Array2;
 //use self::rand::Rng;
 
+#[derive(Clone)]
 pub struct Cell {
     pub top: bool,
     pub bottom: bool,
     pub left: bool,
     pub right: bool,
-    pub ai: usize, // area index
     pub fi: usize, // f-square index
 }
 
 impl Cell {
     pub fn new(top: bool, bottom: bool, left: bool, right: bool,) -> Self {
-        let ai = 0;
         let fi = 0;
-        Self {top, bottom, left, right, ai, fi}
+        Self {top, bottom, left, right, fi}
     }
 }
 
 #[derive(Debug)]
-pub struct Area {
+struct Area {
     xs: Vec<usize>,
     ys: Vec<usize>,
 }
@@ -38,6 +37,18 @@ impl Area {
     pub fn push_cell(&mut self, cell: (usize, usize)) {
         self.xs.push(cell.0 as usize);
         self.ys.push(cell.1 as usize);
+    }
+}
+
+#[derive(Debug)]
+pub struct FSquares {
+    squares: Vec<Area>
+}
+
+impl FSquares {
+    pub fn new() -> Self {
+        let squares = Vec::new();
+        Self {squares: squares}
     }
 }
 
@@ -97,17 +108,41 @@ impl Board {
             col_hashes,
         }
     }
-    pub fn get_score(&self) -> () {
+
+    pub fn get_score(&self) -> usize {
+        //println!("Getting area scores...");
         let areas = self.get_areas();
+        let mut sum: usize;
         for (i, area) in areas.iter().enumerate() {
-            println!("\n{}", i);
+            sum = 0;
             for (&x, &y) in area.xs.iter().zip(&area.ys) {
-                println!("{:>2}, {:>2}", x, y);
+                sum += self.grid[[x, y]].fi;
             }
+            //println!("{:>2}: {}", i, sum);
         }
+        
+        //println!("Getting axis scores...");
+        let row_sums: Vec<usize> = self.grid.axis_iter(ndarray::Axis(0))
+            .map(|row| row.map(|cell| cell.fi).sum())
+            .collect();
+        let col_sums: Vec<usize> = self.grid.axis_iter(ndarray::Axis(1))
+            .map(|col| col.map(|cell| cell.fi).sum())
+            .collect();
+        //println!("{:?}\n{:?}", row_sums, col_sums);
+
+        //println!("Getting blank cells...");
+        let blank_areas = self.get_blank_areas();
+        //println!("Getting product of the length of each blank area...");
+        let mut product: usize = 1;
+        for area in blank_areas.iter() {
+            let length = area.xs.len();
+            product *= length;
+        }
+        //println!("{:?}", blank_areas);
+        product
     }
-    // Sum area scores, rows, and columns 
-    pub fn get_areas(&self) -> Vec<Area> {
+    
+    fn get_areas(&self) -> Vec<Area> {
         let mut areas: Vec<Area> = Vec::new();
         let mut visited = vec![vec![false; self.grid.ncols()]; self.grid.nrows()];
         // Check every cell
@@ -123,7 +158,7 @@ impl Board {
         areas
     }
 
-    fn find_area(&self, i: usize, j: usize, visited: &mut Vec<Vec<bool>>, area: &mut Area) -> () {
+    fn find_area(&self, i: usize, j: usize, visited: &mut Vec<Vec<bool>>, area: &mut Area) {
         if i >= self.grid.nrows() || j >= self.grid.ncols() || visited[i][j] {
             return;
         }
@@ -145,6 +180,44 @@ impl Board {
             self.find_area(i, j - 1, visited, area);
         }
     }
+    
+    fn get_blank_areas(&self) -> Vec<Area> {
+        let mut blank_areas: Vec<Area> = Vec::new();
+        let mut visited = vec![vec![false; self.grid.ncols()]; self.grid.nrows()];
+        // Check every cell as in get_areas
+        for i in 0..self.grid.nrows() {
+            for j in 0..self.grid.ncols() {
+                if self.grid[[i, j]].fi == 0 && !visited[i][j] {
+                    let mut area = Area::new();
+                    self.find_blank_area(i, j, &mut visited, &mut area);
+                    blank_areas.push(area);
+                }
+            }
+        }
+        blank_areas
+    }
+
+    fn find_blank_area(&self, i: usize, j: usize, visited: &mut Vec<Vec<bool>>, area: &mut Area) {
+        if i >= self.grid.nrows() || j >= self.grid.ncols() || visited[i][j] {
+            return;
+        }
+
+        area.push_cell((j, i)); // get_areas and visited does y then x
+        visited[i][j] = true;
+
+        if i + 1 < self.grid.nrows() && self.grid[[i + 1, j]].fi == 0 {
+            self.find_blank_area(i + 1, j, visited, area);
+        }
+        if i > 0 && self.grid[[i - 1, j]].fi == 0 { // i|j=0 should have top|left anyways
+            self.find_blank_area(i - 1, j, visited, area);
+        }
+        if j + 1 < self.grid.ncols() && self.grid[[i, j + 1]].fi == 0 {
+            self.find_blank_area(i, j + 1, visited, area);
+        }
+        if j > 0 && self.grid[[i, j - 1]].fi == 0 {
+            self.find_blank_area(i, j - 1, visited, area);
+        }
+    }
 }
 
 pub struct RandomSearch {
@@ -160,13 +233,35 @@ impl RandomSearch {
         }
     }
 
+    fn attempt_generation(&self) {
+
+    }
+
+    fn generate_squares(&self) -> FSquares {
+        const MAX_ATTEMPTS: usize = 1000;
+        let mut attempts = 0;
+        while attempts < MAX_ATTEMPTS {
+            attempts += 1;
+        }
+        let squares = FSquares::new();
+        squares
+    }
+
     pub fn run(&mut self) {
         //let mut rng = rand::thread_rng();
-        println!("{}", self.board);
-        println!("{:?}", self.board.get_score());
+        let mut squares: FSquares = self.generate_squares();
+        let mut score = self.board.get_score();
+        let mut best = squares;
+        let mut best_score = score;
+        println!("\nBoard:\n{}\nFirst Score: {}", self.board, score);
         for _ in 0..self.iterations {
-            // Generate new board
-            // Get score, compare to previous champion
+            squares = self.generate_squares();
+            score = self.board.get_score();
+            if score > best_score {
+                best_score = score;
+                best = squares;
+            }
         }
+        println!("\nBest:\n{:?}\nScore: {}", best, best_score)
     }
 }
